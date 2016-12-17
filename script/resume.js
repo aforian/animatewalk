329639663cda98b6b95316bf5f9f2b9a0b6ee977
aforian <alexian853@gmail.com>
2016-12-18 04:04:18 +0800

    Add files via upload

--- script/resume.js
+++ script/resume.js
@@ -3,31 +3,25 @@ $(document).ready(function(){
         var wintop = $(window).scrollTop(),
             winheight = $(window).height(),
             htmlheight = $(document).height(),
-            section_num = $(".home-section").length;
-        $(".page-progress").css({"width":(wintop+winheight)/htmlheight*100+"%"});
-
-        for(i=0; i<5; i++){
-            if( wintop >= $(".home-section").eq(i).offset().top && wintop < $(".home-section").eq(i+1).offset().top){
+            percenttop = (wintop+winheight)/htmlheight*100,
+            home_section = $(".home-section");
+            section_num = home_section.length;
+        $(".page-progress").css({"width":percenttop+"%"});
+        for(i=0; i<section_num; i++){
+            j=i+1;
+            if( wintop >= home_section.eq(i).offset().top && wintop < home_section.eq(j).offset().top){
                 $(".nav-item").removeClass('active').eq(i).addClass('active');
             }
         };
-        /*scroll動畫試作*/
         console.log(wintop);
-        var anison = parseFloat($(".title").css("left")),
-            aniparent = parseFloat($(".title").parent().css("width")),
-            anibegin = Math.round(anison/aniparent*100);
-        if(wintop>=100 && wintop<1000){
-            aniresult = 10-(30/900)*(wintop-100);
-            $(".title").css({"left": aniresult+"%"});
-        }else if(wintop<100){
-            $(".title").css({"left": "10"+"%"});
-        }else{
-            $(".title").css({"left": "-20"+"%"});
-        }
-        /*scroll動畫試作end*/
         
+        var right_hand=$(".right-shoulder, .right-arm");
+        if( wintop==0 || percenttop>99.9){
+            right_hand.addClass("sayhi");
+        }else{
+            right_hand.removeClass("sayhi");
+        }
     });
-    
     
     /*導覽列按鈕---*/
     $(".nav-item").click(function(e){
@@ -37,37 +31,33 @@ $(document).ready(function(){
     })
     /*導覽列按鈕---end*/
     
-    /* 人物動態事件----*
-    $(window).scroll(function(){
-        if( wintop >= $("#portfolio").offset().top){
-            $(".right-shoulder, .right-arm").addClass("sayhi");
-        }else{
-            $(".right-shoulder, .right-arm").removeClass("sayhi");
-        };
-    })
-    * 人物動態事件end----*/
-    $(".btn-sayhi").click(function(){
-        $(".right-shoulder, .right-arm").toggleClass("sayhi");
-    });
-    $(".btn-walk").click(function(){
-        $(".left-shoulder, .right-shoulder, .right-leg, .left-leg").toggleClass("active");
-    });
+    /*--滾動人物動態--*/
+    
+    
     $(window).bind('scrollstart', function(){
         $(".left-shoulder, .right-shoulder, .right-leg, .left-leg").addClass("active");
     });
     $(window).bind('scrollstop', function(e){
         $(".left-shoulder, .right-shoulder, .right-leg, .left-leg").removeClass("active");
     })
+    /*--滾動人物動態end--*/
     
-    
-    
-    heightresize();
+    /*--重置人物高度--*/
     $(window).resize(function(){
-        heightresize();
+        $(".boy").heightresize(2);
+        $(".grad-wrap").heightresize(2);
     });
-    function heightresize(){
-        var boy_width;
-        boy_width = $(".boy").width();
-        $(".boy").css({"height":boy_width*2});
-    };
+    (function($){
+        $.fn.extend({
+            heightresize: function(radio){
+                return this.each(function(){
+                    var item_width = $(this).width();
+                    return $(this).css({"height": item_width* radio});
+                });
+            }
+        });
+    })(jQuery);
+    /*--重置人物高度end--*/
+    
+    var s = skrollr.init();
 })
